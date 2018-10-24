b5a751a27e6227277006499024ebf4d17f9af6d3
Marco Aurélio Alano Godinho <marco@trezo.com.br>
2018-10-24 16:28:57 -0300

    fixed php version

--- 7.1/entrypoint.sh
+++ 7.1/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ "$SLOW_LOG" == "true" ]; then
     SLOW_LOG_TIMEOUT=${SLOW_LOG_TIMEOUT:-10}
-    for file in /etc/php/7.0/fpm/pool.d/* ; do
-        echo "slowlog = /var/log/php-fpm/php7.0-slow.log" >> $file
+    for file in /etc/php/7.1/fpm/pool.d/* ; do
+        echo "slowlog = /var/log/php-fpm/php7.1-slow.log" >> $file
         echo "request_slowlog_timeout = ${SLOW_LOG_TIMEOUT}" >> $file
     done
 fi
@@ -12,9 +12,9 @@ if set | grep PROJECT_ROOT ; then
     composer install &
 fi
 
-if php-fpm7.0 -t ; then
-    exec php-fpm7.0 --nodaemonize --fpm-config /etc/php/7.0/fpm/php-fpm.conf
+if php-fpm7.1 -t ; then
+    exec php-fpm7.1 --nodaemonize --fpm-config /etc/php/7.1/fpm/php-fpm.conf
 else
-    echo "php-fpm7.0 config test failed, recheck it"
+    echo "php-fpm7.1 config test failed, recheck it"
 fi
 
