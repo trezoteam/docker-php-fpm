dcc3f04527d6beb9e27f37e844ff440af9d8ef4e
Marco Aurélio Alano Godinho <marco@trezo.com.br>
2019-02-04 14:28:28 -0200

    Use the appropriate UID when running

--- 5.6/entrypoint.sh
+++ 5.6/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 if [ "$SLOW_LOG" == "true" ]; then
     SLOW_LOG_TIMEOUT=${SLOW_LOG_TIMEOUT:-10}
     for file in /etc/php/5.6/fpm/pool.d/* ; do
@@ -7,11 +8,23 @@ if [ "$SLOW_LOG" == "true" ]; then
     done
 fi
 
-if set | grep PROJECT_ROOT ; then
-    cd $PROJECT_ROOT
-    composer install &
-fi
 
+if set | grep PROJECT_ROOT ; then
+    outsider_uid=$(ls -l $PROJECT_ROOT/composer.json | cut -f 3 -d ' ')
+    outsider_gid=$(ls -l $PROJECT_ROOT/composer.json | cut -f 4 -d ' ')
+
+    if [[ $outsider_uid =~ ^[0-9]*$ ]]; then
+        usermod http_user -u $outsider_uid
+    fi
+
+    if [[ $outsider_gid =~ ^[0-9]*$ ]]; then
+        usermod http_group -g $outsider_gid
+    fi
+
+    cd $PROJECT_ROOT
+    su http_user -c 'composer install' &
+fi
+set +x
 if php-fpm5.6 -t ; then
     exec php-fpm5.6 --nodaemonize --fpm-config /etc/php/5.6/fpm/php-fpm.conf
 else
